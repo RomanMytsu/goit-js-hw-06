8aa81e8c7405fbcd179a74e229350208d047afca
RomanMytsu <132835114+RomanMytsu@users.noreply.github.com>
2023-09-01 14:31:54 +0300

    1

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const liArray = [];
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  console.log(item);
+  item.className = "item";
+  item.textContent = ingredient;
+  liArray.push(item);
+});
+
+list.append(...liArray);
